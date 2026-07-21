8393d547bca822eaaac8a9b802fa9bf3138c5c28
Nayeon07 <kimny5726@gmail.com>
2026-07-22 03:14:44 +0900

    fix: token 레포 어노테이션 수정

--- src/main/java/Hampouch/server/domain/auth/repository/RefreshTokenRepository.java
+++ src/main/java/Hampouch/server/domain/auth/repository/RefreshTokenRepository.java
@@ -12,7 +12,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String token);
 
-    @Modifying
+    @Modifying(clearAutomatically = true)
     @Query("UPDATE RefreshToken r SET r.revoked = true WHERE r.userId = :userId AND r.revoked = false")
     void revokeAllByUserId(@Param("userId") Long userId);
 }
