a6b5d079975673a24f725f4ce71bfe3d23347f80
Nayeon07 <kimny5726@gmail.com>
2026-07-29 23:22:32 +0900

    chore: community image errorcode 추가

--- src/main/java/Hampouch/server/global/common/exception/domain/CommunityErrorCode.java
+++ src/main/java/Hampouch/server/global/common/exception/domain/CommunityErrorCode.java
@@ -1,4 +1,17 @@
 package Hampouch.server.global.common.exception.domain;
 
-public class CommunityErrorCode {
+import Hampouch.server.global.common.exception.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommunityErrorCode implements BaseErrorCode {
+
+    COMMUNITY_IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "COMMUNITY_IMAGE_UPLOAD_FAILED", "이미지 업로드 처리 중 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
 }
