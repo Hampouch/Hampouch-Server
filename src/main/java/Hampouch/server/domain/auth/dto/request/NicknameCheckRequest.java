4391aaa267b0f175c408783ac8031c6daf19e232
Nayeon07 <kimny5726@gmail.com>
2026-08-03 16:19:32 +0900

    fix: 닉네임 길이 상한 변경

--- src/main/java/Hampouch/server/domain/auth/dto/request/NicknameCheckRequest.java
+++ src/main/java/Hampouch/server/domain/auth/dto/request/NicknameCheckRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 public record NicknameCheckRequest(
 
         @NotBlank(message = "닉네임을 입력해주세요.")
-        @Size(min = 2, max = 30, message = "닉네임은 2자 이상 30자 이하로 입력해주세요.")
+        @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
         String nickname
 ) {
 }
